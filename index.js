260595dc247b452881cddca15e8235ee932a1c23
Nhat Cao <nhat.cao@niteco.se>
2023-03-22 11:07:05 +0700

    update

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const eris = require("eris");
-const { startsWith, toUpper, upperCase } = require("lodash");
+const { startsWith, toUpper, upperCase, get, assign } = require("lodash");
 const { getPrice } = require("./crawl");
 const bot = new eris.Client(process.env.BOT_TOKEN);
+const prices = {};
+const channelId = "1076529411689562135";
 
 const ensureCommand = (content) => {
   if (!content) return "";
@@ -18,6 +20,7 @@ bot.on("ready", () => {
 
 bot.on("messageCreate", async (msg) => {
   const symbol = ensureCommand(msg.content);
+  console.log("msg.channel.id", msg.channel.id);
 
   if (symbol) {
     try {
@@ -42,15 +45,25 @@ app.get("/status", async (req, res) => {
   return res.status(200).json({ status: "OK" });
 });
 
-app.get("/", async (req, res) => {
-  return res.status(200).json({ status: "OK" });
+app.get("/job", async (req, res) => {
+  const symbol = req.query.symbol ?? "";
+  if (symbol) {
+    const current_price = await getPrice(upperCase(symbol));
+    const last_price = get(prices, symbol, 0);
+    const div = last_price ? current_price / last_price : 2;
+    assign(prices, { [symbol]: current_price });
+    const percent = (div - 1) * 100;
+    console.log("percent", percent);
+    bot.createMessage(
+      channelId,
+      `${upperCase(symbol)}/USDT : ${current_price}`
+    );
+    return res.status(200).json({ status: "OK" });
+  }
+  return res.status(200).json({ status: "Fail" });
 });
 
-app.get("/connect_bot", async (req, res) => {
+app.listen(PORT, async () => {
   await bot.connect();
-  return res.status(200).json({ status: "OK" });
-});
-
-app.listen(PORT, () => {
   console.log("Server is started in " + PORT);
 });
