5f312567fcb1383916f4d0a1fecfaf740f655b13
Nhat Cao <nhat.cao@niteco.se>
2023-04-18 15:12:39 +0700

    update

--- slack.js
+++ slack.js
@@ -7,26 +7,6 @@ const url = process.env.SLACK_BOT;
 // Initialize
 const webhook = new IncomingWebhook(url);
 
-// Send the notification
-(async () => {
-  await webhook.send({
-    attachments: [
-      {
-        color: "#27ae60",
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "This text is <span style='color:#ff0000;'>red</span>, this text is <span style='color:#00ff00;'>green</span>, and this text is <span style='color:#0000ff;'>blue</span>.",
-            },
-          },
-        ],
-      },
-    ],
-  });
-})();
-
 const sendSlack = async (title, description) => {
   await webhook.send({
     attachments: [
